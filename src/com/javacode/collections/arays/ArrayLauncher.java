17be6edbcd0e3dc767bd021467ba4ca798c6443f
2005sojid <2005sojid@mail.ru>
2021-05-02 00:00:44 +0500

    Adding method 'sort'

--- src/com/javacode/collections/arays/ArrayLauncher.java
+++ src/com/javacode/collections/arays/ArrayLauncher.java
@@ -1,11 +1,34 @@
 package com.javacode.collections.arays;
 
+import java.util.Scanner;
+
 public class ArrayLauncher {
     public static void main(String[] args) {
-        int[] myArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
-        for (int j : myArray) {
-            System.out.println(j);
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[8];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Next element: ");
+            int elem = scanner.nextInt();
+            array[i] = elem;
+        }
+        int[] myArray = sort(array);
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Element #" + i + " = " + myArray[i]);
         }
 
     }
+
+    private static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
 }
