2b9b2f57e9785788d457156e5b56c869db369ca4
2005sojid <2005sojid@mail.ru>
2021-05-01 16:26:23 +0500

    First commit, Creating the project

---
+++ src/com/javacode/collections/arays/ArrayLauncher.java
@@ -0,0 +1,9 @@
+package com.javacode.collections.arays;
+
+public class ArrayLauncher {
+    public static void main(String[] args) {
+        int[] myArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+        System.out.println(myArray[1]);
+
+    }
+}
