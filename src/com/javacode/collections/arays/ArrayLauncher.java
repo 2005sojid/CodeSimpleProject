cf17667a8701e042a638aaaaee4ff7a75acc8b5e
2005sojid <2005sojid@mail.ru>
2021-05-01 16:30:20 +0500

    Editing ArrayLauncher

--- src/com/javacode/collections/arays/ArrayLauncher.java
+++ src/com/javacode/collections/arays/ArrayLauncher.java
@@ -3,7 +3,9 @@ package com.javacode.collections.arays;
 public class ArrayLauncher {
     public static void main(String[] args) {
         int[] myArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
-        System.out.println(myArray[1]);
+        for (int j : myArray) {
+            System.out.println(j);
+        }
 
     }
 }
